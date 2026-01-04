bb1f8a8c643cc098a5bcbbb049fe1f624b3557ce
Lucas <lucaspereiradelima2020@gmail.com>
2026-01-04 10:14:08 -0300

    Classe do menu

---
+++ src/com/github/lucasdevrj/principal/MenuPrincipal.java
@@ -0,0 +1,14 @@
+package com.github.lucasdevrj.principal;
+
+import com.github.lucasdevrj.modelos.CartaoDeCredito;
+
+import java.util.Scanner;
+
+public class MenuPrincipal {
+
+    public void exibeMenu() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o limite do cartão: ");
+
+    }
+}
